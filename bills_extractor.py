ca65ba13835c6247418483a50ee56002df495525
Ghonem <mahmoud.gh2016@gmail.com>
2022-10-26 18:11:28 +0200

    automate letters extraction

--- bills_extractor.py
+++ bills_extractor.py
@@ -10,7 +10,7 @@ with open(r'patterns.yml') as file:
 
 
 def read_csv(file):
-    return pd.read_csv(file, sep = ',', header = 0, encoding= 'unicode_escape')
+    return pd.read_csv(file, sep=',', header=0, encoding='unicode_escape')
 
 
 def get_pattern(pattern_txt):
@@ -31,29 +31,35 @@ def get_pattern(pattern_txt):
             pattern += f'[{l.upper()}{l.lower()}]'
     return pattern
 
+
+def get_letters_pattern(pattern_txt):
+    '''
+    :param pattern_txt: the letters pattern written in the YAML
+    :return: pattern after make it non-case sensitive with digits after it
+    '''
+    pattern = ''
+    for l in pattern_txt:
+        # HR  >> "[Hh].?[Rr].?\s?\d+"
+        pattern += f'[{l.upper()}{l.lower()}]' + '.?'
+    return pattern + "\s?\d+"
+
+
 def extract_bills(text):
     if not text or text in ['nan', '', np.nan]:
         return text
 
     bills = []
-    # extracting pattern like H.R. (H.R. 2062)
-    p1 = re.compile("[Hh].?[Rr].?\s?\d+")
-    bills.extend(p1.findall(text))
-
-    # extracting pattern like PL (PL 115-97)
-    p2 = re.compile("[pP].?[lL].?\s?\d+-\d+")
-    bills.extend(p2.findall(text))
-
-    # extracting pattern like S. (S. 1012)
-    p3 = re.compile("[Ss].?\s?\d+")
-    bills.extend(p3.findall(text))
+    # extracting pattern like H.R. using the yaml file
+    for letter in patterns['letters']:
+        p = re.compile(get_letters_pattern(letter))
+        bills.extend(p.findall(text))
 
     # extracting pattern like XXXX ACT (ex: SUBS act)
     bills.extend(re.findall(r'\b[A-Z]+\b\s[Aa][Cc][Tt]', text))
 
     # using yml file to extract other patterns
     for pattern in patterns['patterns']:
-        results = re.findall(get_pattern(pattern),text)
+        results = re.findall(get_pattern(pattern), text)
 
         # remove any overlapping between this case and any other cases
         if results:
